ab1b86d42612e6938c73413fa39a2d8585b9847e
sreasner <steve@LIFTinnovate.com>
2026-04-23 15:08:10 -0500

    Separate Vitest config

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4321,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+  },
+  test: {
+    environment: 'happy-dom',
+    globals: true,
   },
 });
